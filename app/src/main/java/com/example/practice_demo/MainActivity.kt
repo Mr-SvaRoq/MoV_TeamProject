e34af6c55358b908516856199a7c885899999577
klemo1997 <matuskoklemo@gmail.com>
2020-11-18 14:35:12 +0100

    Pridane fragmenty pre login a registraciu
    - login vygenerovany android studiom, je tam vsetko potrebne, uz len dorobit v login akcii REST volanie a podla toho spravit aj registraciu

--- app/src/main/java/com/example/practice_demo/MainActivity.kt
+++ app/src/main/java/com/example/practice_demo/MainActivity.kt
@@ -2,36 +2,21 @@ package com.example.practice_demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.Toast
-import kotlin.random.Random
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.text = "Let`s Roll"
-        rollButton.setOnClickListener {
-//            Toast.makeText(this, "u sucka", Toast.LENGTH_SHORT).show()
-            rollDice()
-        }
+
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 
-    private fun rollDice() {
-        val diceImage: ImageView = findViewById(R.id.id_diceImage)
-        val randomInt = Random.nextInt(6) + 1
-
-        val drawableResource = when (randomInt) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
-        }
-
-       diceImage.setImageResource(drawableResource)
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
     }
 }
